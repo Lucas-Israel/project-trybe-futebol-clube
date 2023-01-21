33bdd24af1ae4f2f41308afd6373916b05a9d8f9
Lucas Israel <lisraelalves92@gmail.com>
2023-01-21 12:28:27 -0300

    testing: another test

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -8,39 +8,43 @@ import { app } from '../app';
 import UserModel from '../database/models/UserModel';
 
 import { Response } from 'superagent';
-const { before, after } = mocha
+// const { before, after } = mocha
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const adminToken = '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+
 describe('Testando a rota login', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+  // before(async () => {
+  //   sinon
+  //     .stub(UserModel, "create")
+  //     .resolves({
+  //       id: 1,
+  //       username: 'Admin',
+  //       role: 'admin',
+  //       email: 'admin@admin.com',
+  //     } as UserModel);
+  // });
 
-  let chaiHttpResponse: Response;
+  // after(()=>{
+  //   (UserModel.create as sinon.SinonStub).restore();
+  // })
 
-  before(async () => {
-    sinon
-      .stub(UserModel, "create")
-      .resolves({
-        id: 1,
-        username: 'Admin',
-        role: 'admin',
-        email: 'admin@admin.com',
-      } as UserModel);
+  afterEach(sinon.restore);
+
+  it('Login feito com sucesso.', async () => {
+    const login = await chai.request(app).post('/login').send({ user: 'Admin', password: adminToken});
+    expect(login.status).to.be.equal(200);
+    expect(login.body).to.be.deep.equal({message: adminToken});
   });
 
-  after(()=>{
-    (UserModel.create as sinon.SinonStub).restore();
-  })
-
-  it('teste do create', async () => {
-    const user = await UserModel.create({username: 'Admin', role: 'admin', email: 'admin@admin.com'})
+  // it('teste do create', async () => {
+  //   const user = await UserModel.create({username: 'Admin', role: 'admin', email: 'admin@admin.com'})
     
-    expect(user).to.be.deep.equal({id: 1, username: 'Admin', role: 'admin', email: 'admin@admin.com'})
-  });
+  //   expect(user).to.be.deep.equal({id: 1, username: 'Admin', role: 'admin', email: 'admin@admin.com'})
+  // });
 
   // it('Seu sub-teste', () => {
   //   expect(false).to.be.eq(true);
