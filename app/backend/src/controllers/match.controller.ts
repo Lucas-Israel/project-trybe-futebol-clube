5a4da36488001a37a072b2a5e53abcc9d264994e
Lucas Israel <lisraelalves92@gmail.com>
2023-01-24 15:46:03 -0300

    add: Match.controller

---
+++ app/backend/src/controllers/match.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import MatchService from '../services/match.service';
+
+export default class MatchController {
+  static async findAll(req: Request, res: Response) {
+    const result = await MatchService.findAll();
+
+    console.log(result);
+
+    res.status(result.status).json(result.message);
+  }
+}
