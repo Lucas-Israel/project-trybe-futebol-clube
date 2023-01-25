c6f4d91e9251cb59b13fa4a1c93240010e3ed43b
Lucas Israel <lisraelalves92@gmail.com>
2023-01-25 17:52:25 -0300

    added: validation for same team

--- app/backend/src/routes/match.routs.ts
+++ app/backend/src/routes/match.routs.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import validateMatchWithSameTeam from '../middlewares/match.validation';
 
 import MatchController from '../controllers/match.controller';
 
 const router = Router();
 
 router.get('/', MatchController.findAll);
-router.post('/', MatchController.createMatchInProgress);
+router.post('/', validateMatchWithSameTeam, MatchController.createMatchInProgress);
 router.patch('/:id/finish', MatchController.finishMatchInProgress);
 
 export default router;
