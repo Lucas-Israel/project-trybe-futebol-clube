f2506e6fd980d66999ad4dc7f313e2ac1e43d148
Lucas Israel <lisraelalves92@gmail.com>
2023-01-25 12:59:46 -0300

    add: test for query string

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -5,6 +5,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import MatchModel from '../database/models/MatchModel';
+import { queryReturn, queryReturnInProgress } from './utils/matchVariables';
 
 import { Response } from 'superagent';
 
@@ -12,66 +13,21 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const toSend = [
-  {
-    id: 1,
-    homeTeamId: 16,
-    homeTeamGoals: 1,
-    awayTeamId: 8,
-    awayTeamGoals: 1,
-    inProgress: 0,
-    homeTeam: { teamName: 'São Paulo' },
-    awayTeam: { teamName: 'Grêmio' }
-  },
-  {
-    id: 2,
-    homeTeamId: 9,
-    homeTeamGoals: 1,
-    awayTeamId: 14,
-    awayTeamGoals: 1,
-    inProgress: 0,
-    homeTeam: { teamName: 'Internacional' },
-    awayTeam: { teamName: 'Santos' }
-  },
-]
-
-const expectedReturn = [
-  {
-    id: 1,
-    homeTeamId: 16,
-    homeTeamGoals: 1,
-    awayTeamId: 8,
-    awayTeamGoals: 1,
-    inProgress: 0,
-    homeTeam: {
-      teamName: "São Paulo"
-    },
-    awayTeam: {
-      teamName: "Grêmio"
-    }
-  },
-  {
-    id: 2,
-    homeTeamId: 9,
-    homeTeamGoals: 1,
-    awayTeamId: 14,
-    awayTeamGoals: 1,
-    inProgress: 0,
-    homeTeam: {
-      teamName: "Internacional"
-    },
-    awayTeam: {
-      teamName: "Santos"
-    }
-  }
-]
-
 describe('Testando a rota /matches', () => {
   it('Retorna uma lista com informações das partidas ao acessar a rota GET /matches ', async () => {
-    sinon.stub(MatchModel, 'findAll').resolves(toSend as any);
+    sinon.stub(MatchModel, 'findAll').resolves(queryReturn as any);
     const result = await chai.request(app).get('/matches')
 
     expect(result.status).to.be.equal(200);
-    expect(result.body).to.be.deep.equal(expectedReturn);
+    expect(result.body).to.be.deep.equal(queryReturn);
+    sinon.restore();
+  })
+
+  it('Retorna uma lista com informações das partidas em andamento ao acessar a rota GET /matches', async () => {
+    // sinon.stub(MatchModel, 'findAllInProgress').resolves(queryReturnInProgress as any);
+    const result = await chai.request(app).get('/matches/matches?inProgress=true');
+
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.deep.equal(queryReturnInProgress);
   })
 })
---
+++ app/backend/src/tests/utils/matchVariables.ts
@@ -0,0 +1,61 @@
+export const queryReturn = [
+  {
+    id: 1,
+    homeTeamId: 16,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    homeTeam: {
+      teamName: "São Paulo"
+    },
+    awayTeam: {
+      teamName: "Grêmio"
+    }
+  },
+  {
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    homeTeam: {
+      teamName: "Internacional"
+    },
+    awayTeam: {
+      teamName: "Santos"
+    }
+  }
+]
+
+export const queryReturnInProgress = [
+  {
+    id: 41,
+    homeTeamId: 16,
+    homeTeamGoals: 2,
+    awayTeamId: 9,
+    awayTeamGoals: 0,
+    inProgress: 1,
+    homeTeam: {
+      teamName: "São Paulo"
+    },
+    awayTeam: {
+      teamName: "Internacional"
+    }
+  },
+  {
+    id: 42,
+    homeTeamId: 6,
+    homeTeamGoals: 1,
+    awayTeamId: 1,
+    awayTeamGoals: 0,
+    inProgress: 1,
+    homeTeam: {
+      teamName: "Ferroviária"
+    },
+    awayTeam: {
+      teamName: "Avaí/Kindermann"
+    }
+  }
+]
